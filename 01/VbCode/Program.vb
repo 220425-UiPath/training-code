a0b61a437fbc177ddf6d2caa6aec7646df6e694f
Pushpinder <pushpinder.kaur@revature.com>
2022-04-28 18:00:24 -0400

    VB.Net Demo

--- 01/VbCode/Program.vb
+++ 01/VbCode/Program.vb
@@ -1,7 +1,16 @@
-Imports System
+Imports System 'importing namespace
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+Module Program 'Module or clas holds the program data and procedures
+    Sub Main(args As String()) 'procedure -> behavior of the program
+        ' Console.Write("Please enter your name ")
+        ' 'Variable
+        ' Dim name as String = Console.ReadLine()'takes input from the user
+        ' Console.WriteLine($"Hello {name}")'print hello world
+
+        ' Type Conversion
+        Dim a as Integer = 0'String = "10"
+        Dim b as Integer = 10'String = "20"
+        'Console.Write(CInt(a)+CInt(b))
+        Console.WriteLine(CBool(b))
     End Sub
 End Module
