346e8734482888f0f02b34fd1f0aff330fc5f7ca
Pushpinder <pushpinder.kaur@revature.com>
2022-04-29 13:23:57 -0400

    Sub with parameters

--- 01/VbCode/Calculator.vb
+++ 01/VbCode/Calculator.vb
@@ -1,13 +1,8 @@
 Namespace Global.Mathematics   
    public Class Calculator
         'Procedure to add 2 numbers
-        Sub Addition()
-            Dim a,b,c as Integer 'variable declaration
-            Console.Write("Please enter a number ")
-                a=Console.ReadLine() 'Variable initialization
-            Console.Write("Please enter another number ")    
-                b=Console.ReadLine()
-            c=a+b 'Expression to compute addition
+        Sub Addition(a as Integer, b as Integer)
+            Dim c as Integer = a+b 'Expression to compute addition
             Console.Write($"{a} + {b} = {c}") 'printing the result to the user
         End Sub        
     End Class  
--- 01/VbCode/Program.vb
+++ 01/VbCode/Program.vb
@@ -1,16 +1,24 @@
 Imports System 'importing namespace
 imports Mathematics
-Namespace VbCode
 
+Namespace VbCode
 Module Program 'Module or class holds the program data and procedures
     'Main Procedure is the entry point of the program 
- Sub Main(args As String()) 'procedure -> behavior of the program, args here is command line arguement which we can pass with dotnet run command
-        Console.WriteLine($"First Command line args - {args(0)},{vbCrLf} Second Command line args - {args(1)}")
-        Dim obj as Mathematics.Calculator = new Calculator()
-        obj.Addition() 'Calling the procedure
-        
+    Sub Main(args As String()) 'procedure -> behavior of the program, args here is command line arguement which we can pass with dotnet run command
+            'Console.WriteLine($"First Command line args - {args(0)},{vbCrLf} Second Command line args - {args(1)}")
+            TakeInput()   
     End Sub
+    Sub TakeInput()
+            Dim a,b as Integer 'variable declaration
+            Console.Write("Please enter a number ")
+            a=Console.ReadLine() 'Variable initialization
+            Console.Write("Please enter another number ")    
+            b=Console.ReadLine()
 
+            Dim obj as Mathematics.Calculator = new Calculator()
+            obj.Addition(a,b) 'Calling the Sub, passing the parameters by value
+    End Sub
+    
     Sub LearningDatatypes()
         ' Console.Write("Please enter your name ")
         ' 'Variable
@@ -22,7 +30,6 @@ Module Program 'Module or class holds the program data and procedures
         Dim b as Integer = 10'String = "20"
         'Console.Write(CInt(a)+CInt(b))
         Console.WriteLine(CBool(b))
-    End Sub
-    
+    End Sub    
 End Module
 End Namespace
